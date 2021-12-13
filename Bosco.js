001bff06b1ab2d622b536ce2d46968126d65a9bb
It's me levi ser <92271735+Kailas210@users.noreply.github.com>
2021-12-14 00:21:00 +0530

    Update Bosco.js

--- Bosco.js
+++ Bosco.js
@@ -861,7 +861,7 @@ const ftrol = {
         }
          if (isGroup && isAntiLink && !isOwner && !isGroupAdmins && isBotGroupAdmins){
             if (budy.match(/(https:\\chat.whatsapp.com)/gi)) {
-                reply(`*🚧GROUP LINK DETECTOR🚧*\n\n_To Any Links Send This Group You Will Kicked_`)
+                reply(`*🚧GROUP LINK DETECTOR🚧 *\n\n_To Any Links Send This Group You Will Kicked_`)
                 bosco.groupRemove(from, [sender])
             }
         }
@@ -984,7 +984,8 @@ ${jmn} -  ${jmo}\n${week} - ${calender}
      function _0x4663(){var _0x2fc8bc=['61360RbdMuw','1938303OzLjeN','659960nzjTUM','404766EJGGBI','7WhscAJ','997400vvejgD','1297674CcBmhI','610998dTuyrA','5IPhDWS'];_0x4663=function(){return _0x2fc8bc;};return _0x4663();}function _0x1231(_0x40cb45,_0x55ff98){var _0x4663dc=_0x4663();return _0x1231=function(_0x1231ee,_0x440ba1){_0x1231ee=_0x1231ee-0x166;var _0x2eb6a7=_0x4663dc[_0x1231ee];return _0x2eb6a7;},_0x1231(_0x40cb45,_0x55ff98);}(function(_0x4d6264,_0xc43f28){var _0x4f3c9d=_0x1231,_0xf81e96=_0x4d6264();while(!![]){try{var _0x15833d=parseInt(_0x4f3c9d(0x16b))/0x1+-parseInt(_0x4f3c9d(0x16e))/0x2+-parseInt(_0x4f3c9d(0x169))/0x3+-parseInt(_0x4f3c9d(0x16d))/0x4*(-parseInt(_0x4f3c9d(0x16a))/0x5)+parseInt(_0x4f3c9d(0x168))/0x6+-parseInt(_0x4f3c9d(0x166))/0x7*(parseInt(_0x4f3c9d(0x167))/0x8)+parseInt(_0x4f3c9d(0x16c))/0x9;if(_0x15833d===_0xc43f28)break;else _0xf81e96['push'](_0xf81e96['shift']());}catch(_0x375167){_0xf81e96['push'](_0xf81e96['shift']());}}}(_0x4663,0x1f128),groupBot='ʜʏ ᴅᴜᴅᴇ....☕︎',catlo(groupBot));
     break
     case 'ownermenu':
-    owner1 =`
+    bosco1 = await bosco.prepareMessage(from, denis, location, {thumbnail: denis})
+              bosco2 = bosco1.message["ephemeralMessage"] ? bosco1.message.ephemeralMessage : bosco1
 ▢☘︎ *𝙾𝚆𝙽𝙴𝚁 - 𝙼𝙴𝙽𝚄* ☘︎▢
 
 
@@ -1028,7 +1029,12 @@ ${jmn} -  ${jmo}\n${week} - ${calender}
 
 ▢☘︎ ${prefix}readall
 `
- catlo(owner1)
+ menubutton = [{buttonId:`nice`,buttonText:{displayText:'ɴɪᴄᴇ𖠌'},type:1},
+                   {buttonId:`${prefix}owner`,buttonText:{displayText:'ᴏᴡɴᴇʀ♔︎'},type:1}
+]
+ menumessage = { contentText: `${teks}`, footerText: `©ⁱᵗˢᵐᵉᗷᗴᒪᑕO𝓼𝓮𝓻♔︎¹⁰`, buttons: menubutton, headerType: 6, locationMessage: bosco2.message.locationMessage}
+ bosco.sendMessage(from, menumessage, MessageType.buttonsMessage)
+   
    break
    case 'groupmenu':
       group1 = `
